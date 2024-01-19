432e98d0c6d54f8555bc045f2b30c9dd818d107d
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-19 11:21:16 +0100

    Import recent data

--- ReadIn.R
+++ ReadIn.R
@@ -1,5 +1,5 @@
-raw <- read.csv("DownloadedMODISLAI/HidegvizLAI4days-MCD15A3H-006-results.csv")
+raw <- read.csv("Hidegviz-valley-MCD15A3H-061-results.csv")
 names(raw)
 library(xts)
 ## 8th variable is LAI, 3rd is Date with name is better
-LAI.x <- xts(raw[,"MCD15A3H_006_Lai_500m"], as.Date(as.character(raw[,"Date"])))
+LAI.x <- xts(raw[,"MCD15A3H_061_Lai_500m"], as.Date(as.character(raw[,"Date"])))
